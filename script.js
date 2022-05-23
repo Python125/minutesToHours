abf2f0f63949e70ffd45091e5f94e7dcefce2714
Python125 <91922582+Python125@users.noreply.github.com>
2022-05-23 12:02:06 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function Convert(num) {
+  h = Math.floor(num/60);
+  m = num % 60;
+  return(h + "hours"+":" + m + "minutes").toString();
+
+}
+
+console.log(Convert(142));
+
+function convertToHours() {
+  
+}
