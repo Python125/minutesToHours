5d37b79cae15e3ce1269d1184cd29ea7cea2c9f2
Python125 <91922582+Python125@users.noreply.github.com>
2022-05-23 16:40:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function Convert(num) {
-  h = Math.floor(num/60);
-  m = num % 60;
-  return(h + "hours"+":" + m + "minutes").toString();
-
+function timeConvert(n) {
+let num = n;
+let hours = (num / 60);
+let rhours = Math.floor(hours);
+let minutes = (hours - rhours) * 60;
+let rminutes = Math.round(minutes);
+return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
 }
 
-Convert(90);
+console.log(timeConvert(90));
